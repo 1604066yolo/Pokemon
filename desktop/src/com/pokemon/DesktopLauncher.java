85bf49730726da44f483d64348e05b17f0afab27
Karthik Raman <karthikraman2011@gmail.com>
2022-09-23 12:47:33 -0400

    Test Commit 2

---
+++ desktop/src/com/pokemon/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.pokemon;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.pokemon.PokemonMain;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Pokemon");
+		new Lwjgl3Application(new PokemonMain(), config);
+	}
+}
